f38aecd2357047ecd365d2a4e83309f79114540c
aloomdar <aqanoongo@gmail.com>
2024-06-06 13:25:47 -0400

    commit

---
+++ summer2024/hw1/31609560_Problem4.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+function menu24()
+{
+start=0
+
+currentTime=$(date +%s)
+pastDay=$((currentTime - 86400))
+echo "Enter the name of a file or subdirectory"
+echo "If you wish to exit, press Ctrl+C"
+for item in *
+do
+	modifiedTime=$(stat -c %Y "$item")
+	if [ $modifiedTime -ge $pastDay ]; then
+		echo "$((start = start + 1))) $item"
+	fi
+done
+}
+
+function traverse()
+{
+if [ -f "$1" ]; then
+	head "$1"
+	echo "Would you like to display more? (Y/n)"
+	read answer
+	start=0
+	while [ $answer = "Y" ]; do
+		tail -n +$((start = start + 10)) "$1"
+		echo "Would you like to display more? If empty, end of file is reached. (Y/n)"
+		read answer
+	done
+	menu24
+	read selection
+	traverse "${selection}"
+fi
+if [ -d "$1" ]; then
+	cd "$1"
+	menu24
+	read selection
+	traverse "$selection"
+fi
+}
+
+echo "Enter a directory name: "
+read dirname
+
+if [ ! -e "${dirname}" ]; then
+	echo "Directory does not exist. Would you like to create directory? (Y/n)"
+	read choice
+	if [ $choice == "Y" ]; then
+		mkdir "${dirname}"
+		exit
+	else
+		exit
+	fi
+fi
+
+cd "${dirname}"
+
+start=0
+ 
+echo "Enter the name of a file or subdirectory"
+echo "If you wish to exit, press Ctrl+C"
+for item in *
+do  
+	echo "$((start = start + 1))) $item"
+done
+
+read selection
+
+traverse "${selection}"
