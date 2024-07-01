2979c717da13b0b6050342d7e8a5e39d768b5c7f
aloomdar <aqanoongo@gmail.com>
2024-07-01 15:30:17 -0400

    commits

--- summer2024/hw5/31609560_Problem1.c
+++ summer2024/hw5/31609560_Problem1.c
@@ -1,9 +1,24 @@
 #include <stdio.h>
-#include <string.h>
 
-int main(int argc, char *argv[]){
-
-    printf("argv \t %p \t %p", &argv[0], &argv[0]);
-    return 0;
-    
+void hexPrint(unsigned char* arg){
+	for(int i = 7; i >= 0; i--){
+		printf("%02hhx ", *arg+i);
+	}
 }
+
+int main(int argc, char* argv[])
+{
+	unsigned char *p = (unsigned char *) &argv;
+	printf("Argument count: %d\n", argc);
+	printf("argv \t| %p \t| %p\n\n", p, p);
+	for(int i = 0; i < argc; i++){
+		printf("argv[%d] | ", i);
+		unsigned char* p2 = (unsigned char *) argv[i];
+		for(int j = 7; j >= 0; j--){
+			printf("%02hhx ", *(p2 + j));
+		}
+		printf("| %p\n", &p+i);
+	}
+	return 0;
+}
+
